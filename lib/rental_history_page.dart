a436cf991cece72dc3b5ddd50a7f80fddc813d62
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2025-11-19 20:21:48 +0700

    menambahkan halaman riwayat

---
+++ lib/rental_history_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RentalHistoryPage extends StatefulWidget {
+  const RentalHistoryPage({super.key});
+
+  @override
+  State<RentalHistoryPage> createState() => _RentalHistoryPageState();
+}
+
+class _RentalHistoryPageState extends State<RentalHistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
