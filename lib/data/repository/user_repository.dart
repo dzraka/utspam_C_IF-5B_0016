810a50e880514216ca6c03f811bd1d2ff25930ac
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2025-11-20 21:04:25 +0700

    menambahkan user_repository.dart

---
+++ lib/data/repository/user_repository.dart
@@ -0,0 +1,30 @@
+import 'package:car_rent_app/data/db/user_dao.dart';
+import 'package:car_rent_app/data/model/user.dart';
+
+class UserRepository {
+  final userDao = UserDao();
+
+  Future<int> registertUser(User user) async {
+    return await userDao.insertUser(user);
+  }
+
+  Future<User?> getUserById(int id) async {
+    return await userDao.getUserById(id);
+  }
+
+  Future<User?> getUserByUsername(String username) async {
+    return await userDao.getUserByUsername(username);
+  }
+
+  Future<User?> login(String username, String password) async {
+    final user = await userDao.getUserByUsername(username);
+
+    if (user == null) return null;
+
+    if (user.password == password) {
+      return user;
+    }
+
+    return null;
+  }
+}
